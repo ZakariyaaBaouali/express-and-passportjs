c4a8f03e2b15d459cfd9b127c9a0ad0896cc5496
zakaria <zakariabaouali@outlook.com>
2024-05-26 21:20:57 +0100

    update passsport config

--- passport/index.ts
+++ passport/index.ts
@@ -13,19 +13,18 @@ export const initializePassport = () => {
       (accessToken, refreshToken, profile, done) => {
         console.log(accessToken);
         console.log(refreshToken);
+        console.log(profile);
         done(null, profile);
       }
     )
   );
 
   passport.serializeUser((user, done) => {
-    //create a cookie with a user id
     console.log("inside user serialize");
     done(null, user);
   });
 
   passport.deserializeUser((id, done) => {
-    //take the user id from the cookie send by the browser
     console.log("inside user deserialize");
     const user = { id };
     done(null, user);
