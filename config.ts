5bb09e202c4ea747da1fddf1aae38f14c89f7348
zakaria <zakariabaouali@outlook.com>
2024-05-26 15:57:06 +0100

    complate the express server setup

---
+++ config.ts
@@ -0,0 +1,5 @@
+export const PORT = process.env.PORT;
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+export const COOKIE_SECRET_KEY = process.env.COOKIE_SECRET_KEY || "";
+export const CLIENT_URI = process.env.CLIENT_URI;
--- server.ts
+++ server.ts
@@ -1,5 +1,24 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import env from "dotenv";
+import cookie from "cookie-session";
+import { COOKIE_SECRET_KEY, PORT } from "./config";
+import passport from "passport";
 
 const app = express();
 env.config();
+
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(
+  cookie({
+    maxAge: 24 * 60 * 60 * 1000,
+    name: "my-session",
+    keys: [COOKIE_SECRET_KEY],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen(PORT, () =>
+  console.log(`Server running successfully on port ${PORT} 🚀🚀🚀`)
+);
