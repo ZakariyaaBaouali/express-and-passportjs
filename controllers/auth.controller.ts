90b7e916adfa24a3f916d309f608a386e7584ca4
zakaria <zakariabaouali@outlook.com>
2024-05-26 21:19:44 +0100

    complete the auth controller

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,14 +1,15 @@
 import { Router, Request, Response } from "express";
-import passport, { use } from "passport";
+import passport from "passport";
 import { CLIENT_URI } from "../config";
 
 const route = Router();
 
 route.get("/login/failed", (req: Request, res: Response) => {
-  return res.status(401).send({
+  console.log({
     success: false,
     message: "Failed to login with google",
   });
+  return res.status(401).redirect(`${CLIENT_URI}/auth/failed`);
 });
 
 route.get("/login/success", (req: Request, res: Response) => {
@@ -16,7 +17,7 @@ route.get("/login/success", (req: Request, res: Response) => {
   if (req.user) user = req.user;
   return res.status(200).send({
     success: true,
-    user,
+    user: user,
     cookies: req.cookies,
   });
 });
@@ -31,9 +32,14 @@ route.get(
 route.get(
   "/google/callback",
   passport.authenticate("google", {
-    successRedirect: CLIENT_URI,
-    failureRedirect: "/login/failed",
-  })
+    failureRedirect: "/auth/login/failed",
+  }),
+  (req: Request, res: Response) => {
+    console.log("Google was here 🚀🚀" + req.user);
+    const CLIENT_SUCCESS_PATH = `${CLIENT_URI}/auth/success`;
+    console.log(CLIENT_SUCCESS_PATH);
+    return res.status(200).redirect(CLIENT_SUCCESS_PATH);
+  }
 );
 
 export default route;
