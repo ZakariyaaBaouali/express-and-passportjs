131ea8d737161e86c143e4a550144eefa1a6c1f4
zakaria <zakariabaouali@outlook.com>
2024-05-26 21:21:19 +0100

    complete the server auth

--- server.ts
+++ server.ts
@@ -1,27 +1,17 @@
-import express, { urlencoded } from "express";
-import cookie from "cookie-session";
-import { COOKIE_SECRET_KEY, PORT } from "./config";
-import passport from "passport";
+import express, { Request, Response } from "express";
 import { initializePassport } from "./passport";
 import authController from "./controllers/auth.controller";
+import { PORT, settupMiddlewares } from "./config";
 
 const app = express();
 
-app.use(express.json());
-app.use(urlencoded({ extended: true }));
-app.use(
-  cookie({
-    maxAge: 24 * 60 * 60 * 1000,
-    name: "my-session",
-    keys: [COOKIE_SECRET_KEY],
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
+settupMiddlewares(app);
 initializePassport();
-
 app.use("/auth", authController);
+
+app.get("/health-check", (req: Request, res: Response) => {
+  return res.status(200).send(`Server working`);
+});
 
 app.listen(PORT, () =>
   console.log(`Server running successfully on port ${PORT} 🚀🚀🚀`)
